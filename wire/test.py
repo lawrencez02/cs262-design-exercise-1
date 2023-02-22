937e5f36b20cf88f11dda0569874573c41319c74
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2023-02-21 21:31:34 -0500

    Add tests

--- wire/test.py
+++ wire/test.py
@@ -4,11 +4,13 @@ import sys
 import time
 import client, server
 import threading
+import inspect
+import os
 from constants import *
 
 
 test_port = 2622
-server1 = server.Server("", test_port, logs=False)
+server = server.Server("", test_port, logs=False)
 client1 = client.Client("localhost", test_port)
 client2 = client.Client("localhost", test_port)
 user_input1 = client.UserInput(client1)
@@ -32,18 +34,59 @@ class TestLogin(unittest.TestCase):
         self.assertEqual(print_it(user_input1.do_login, "username.special   password"), "Special characters not allowed in usernames. Please try again!")
         self.assertEqual(print_it(user_input1.do_login, "u" * (MAX_LENGTH + 1) + " password"), "Username or password is too long. Please try again!")
 
-    def test_login_privilege_error(self):
+    def test_login_server_errors(self):
         self.assertEqual(print_it(user_input1.do_register, "user1 password1"), "user1 successfully registered, please log in!")
         self.assertEqual(server.users["user1"], "password1")
+        self.assertEqual(server.active_conns, {})
+        self.assertEqual(print_it(user_input1.do_login, "user1 password2"), "Incorrect username or password. Please try again!")
+        self.assertEqual(print_it(user_input1.do_login, "user234 password234"), "Incorrect username or password. Please try again!")
 
-    def test_successful_login(self):
-        pass
+    def test_login_server_errors_2_and_success(self):
+        self.assertEqual(print_it(user_input1.do_register, "user2 password2"), "user2 successfully registered, please log in!")
+        self.assertEqual(server.users["user2"], "password2")
+        self.assertEqual(print_it(user_input1.do_login, "user1 password1"), "Logged in as user1!")
+        self.assertEqual(print_it(user_input2.do_login, "user2 password2"), "Logged in as user2!")
+
+# function to identify what is a test class object in this file / module
+def isTestClass(x):
+    return inspect.isclass(x) and issubclass(x, unittest.TestCase)
+# function to identify what is a test function in this file / module
+def isTestFunction(x):
+    return inspect.isfunction(x) and x.__name__.startswith("test")
+
+def suite(): 
+    # get current module 
+    module = sys.modules[__name__]
+    # get a list of (class names, class object) tuples for all the classes defined above (in current module)
+    testClasses = [tup for tup in inspect.getmembers(module, isTestClass)]
+    # sort the classes by line number
+    testClasses.sort(key=lambda t: inspect.getsourcelines(t[1])[1])
+
+    testSuite = unittest.TestSuite()
+
+    # for each class
+    for testClass in testClasses:
+        # get list of (testFunctionName,testFunction) tuples in that class
+        classTests = [
+            tup for tup in
+            inspect.getmembers(testClass[1], isTestFunction)
+        ]
+        # sort by line number
+        classTests.sort(key=lambda t: inspect.getsourcelines(t[1])[1])
+        # for each class, create TestCase instances
+        # add to testSuite;
+        for test in classTests:
+            testSuite.addTest(testClass[1](test[0]))
+
+    return testSuite
 
 if __name__ == '__main__':
-    threading.Thread(target=server1.run).start()
+    threading.Thread(target=server.run).start()
     threading.Thread(target=client1.send).start()
     threading.Thread(target=client1.receive).start()
     threading.Thread(target=client2.send).start()
     threading.Thread(target=client2.receive).start()
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+    os._exit(1)
 
